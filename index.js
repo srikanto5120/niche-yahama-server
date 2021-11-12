5f0c613dacf487d33ad2e0d54c729fbf141ed2cf
srikanto5120 <shreekantaray@gmail.com>
2021-11-13 04:42:02 +0600

    commit

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ async function run() {
     const database = client.db("yamaha");
     const bikesCollection = database.collection("bikes");
     const userOrderCollection = database.collection("orderBikes");
+    const userCollection = database.collection("users");
+    const reviewCollection = database.collection("review");
     // get limit  data
     app.get("/bikes", async (req, res) => {
       const result = await bikesCollection.find({}).limit(6).toArray();
@@ -38,18 +40,57 @@ async function run() {
     });
 
     // order get data
-    app.use("/bikes/:orderId", async (req, res) => {
+    app.get("/bikes/:orderId", async (req, res) => {
       const id = req.params.orderId;
       const cursor = { _id: ObjectId(id) };
       const result = await bikesCollection.findOne(cursor);
       res.send(result);
     });
-    // post data
+    // get user data
+    app.get("/users/:userDataId", async (req, res) => {
+      const id = req.params.userDataId;
+      const cursor = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(cursor);
+      res.send(result);
+    });
+
+    // get order data
+    app.get("/orderBike", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = userOrderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+    // get review data
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // post users data
+    app.post("/users", async (req, res) => {
+      const result = await userCollection.insertOne(req.body);
+    });
+
+    // post order  data
     app.post("/orderBikes", async (req, res) => {
       userOrderCollection.insertOne(req.body).then((result) => {
         res.send(result.insertedId);
-        console.log(result);
       });
+    });
+
+    // post review data
+    app.post("/review", async (req, res) => {
+      reviewCollection.insertOne(req.body).then((result) => {
+        res.send(result.insertedId);
+      });
+    });
+    app.delete("/orderBikes/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const cursor = { _id: ObjectId(id) };
+      const result = await userOrderCollection.deleteOne(cursor);
     });
 
     console.log("bikes");
